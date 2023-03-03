afba820e0dc8f32d3bc012d06b61dccffd60adb3
Ted <andrei.teodor21@yahoo.ro>
2023-03-03 14:09:51 +0100

    Added controller for PinnedTasks

--- TasksApp/TasksAPI/Controllers/PinnedTasksController.cs
+++ TasksApp/TasksAPI/Controllers/PinnedTasksController.cs
@@ -1,18 +1,22 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using TasksAPI.Data;
 using TasksAPI.DTOs;
+using TasksAPI.Models;
 
 namespace TasksAPI.Controllers;
 [Route("api/[controller]")]
 [ApiController]
 public class PinnedTasksController : ControllerBase
 {
-    IPinnedTaskRepository _pinnedTaskRepository;
+    private readonly IPinnedTaskRepository _pinnedTaskRepository;
+    private readonly IMapper _mapper;
 
-    public PinnedTasksController(IPinnedTaskRepository pinnedTaskRepository)
+    public PinnedTasksController(IPinnedTaskRepository pinnedTaskRepository, IMapper mapper)
     {
         _pinnedTaskRepository = pinnedTaskRepository;
+        _mapper = mapper;
     }
 
     [HttpGet]
@@ -26,6 +30,86 @@ public class PinnedTasksController : ControllerBase
         return NotFound();
         }
 
-        return Ok(pinnedTask);
+        return Ok(_mapper.Map<PinnedTaskDto>(pinnedTask));
     }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<PinnedTaskDto>>> GetAllPinnedTasksAsync()
+    {
+        var pinnedTasks = await _pinnedTaskRepository.GetAllPinnedTasksAsync();
+
+        if(pinnedTasks == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(_mapper.Map<IEnumerable<PinnedTaskDto>>(pinnedTasks));
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<int>> InsertPinnedTaskAsync(PinnedTaskDto pinnedTaskDto)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var pinnedTask = _mapper.Map<PinnedTask>(pinnedTaskDto);
+
+        var pinnedTaskId = await _pinnedTaskRepository.InsertPinnedTaskAsync(pinnedTask);
+
+        if (pinnedTaskId == 0)
+        {
+            return BadRequest();
+        }
+
+        return Ok(pinnedTaskId);
+    }
+
+    [HttpDelete]
+    [Route("{id}")]
+    public async Task<ActionResult> DeletePinnedTaskAsync(int id)
+    {
+        if(await _pinnedTaskRepository.GetPinnedTaskByIdAsync(id) == null)
+        {
+            return NotFound();
+        }
+
+        if(! await _pinnedTaskRepository.DeletePinnedTaskAsync(id))
+        {
+            return BadRequest();
+        }
+
+        return NoContent();
+    }
+
+    [HttpPut]
+    [Route("updatepinnedtask/{id}")]
+    public async Task<ActionResult> UpdatePinnedTaskAsync(int id, PinnedTaskDto pinnedTaskDto)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var pinnedTask = _mapper.Map<PinnedTask>(pinnedTaskDto);
+
+        if(id == pinnedTask.Id)
+        {
+            return Unauthorized();
+        }
+
+        if (await _pinnedTaskRepository.GetPinnedTaskByIdAsync(pinnedTask.Id) == null)
+        {
+            return NotFound();
+        }
+
+        if(! await _pinnedTaskRepository.UpdatePinnedTaskAsync(pinnedTask))
+        {
+            return BadRequest();
+        }
+
+        return NoContent();
+    }
+
 }
